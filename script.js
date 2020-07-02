5127c11024b0b3f5754bda57e8e8739fd592b19e
Tomas <tomasfreire@gmail.com>
2020-07-02 16:29:01 -0300

    some basic style

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const textarea = document.getElementById("textarea");
 const output = document.getElementById("output");
 const button = document.getElementById("start-btn");
 
-button.onclick = function(){
+function print(){
   if(textarea.value){
     const arr = textarea.value.split(" ");
     let index = 0;
@@ -11,6 +11,6 @@ button.onclick = function(){
         output.innerHTML = arr[index];
         index++ 
       }
-    }, 1000)
+    }, 200)
   }   
 };
